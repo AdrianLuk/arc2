5b8d80c7c3cc1c60274076b2bcca1a10ed7f87f8
Adrian <lukabaseball@hotmail.com>
2020-11-10 20:30:08 -0500

    insta token update nov 10 2020

--- dist-product/js/theme.js
+++ dist-product/js/theme.js
@@ -6852,147 +6852,141 @@
 })();
 
 jQuery().ready(function ($) {
-    var homeSwiper = new Swiper(".homepage-swiper", {
-        slidesPerView: 1,
-        loop: true,
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        speed: 1500,
-        autoplay: {
-            delay: 4000,
-        },
+  var homeSwiper = new Swiper('.homepage-swiper', {
+    slidesPerView: 1,
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    speed: 1500,
+    autoplay: {
+      delay: 4000,
+    },
+  });
+  $(function () {
+    'use strict';
+    $('[data-toggle="offcanvas"]').on('click', function () {
+      $('.offcanvas-collapse').toggleClass('open');
     });
-    $(function () {
-        "use strict";
-        $('[data-toggle="offcanvas"]').on("click", function () {
-            $(".offcanvas-collapse").toggleClass("open");
-        });
-    });
-    var token =
-            "IGQVJXWlFkc29yVG5Vd0tWM1pmMExJQ1RfSklzdjZABZADVtODVfWE9hdWlLeUtkMzlPOTNfZAU9keFVwcWwxNnJiWVFBaXNIWmpqNUpTWkk4T1FFNkRybmQwVWJ4N3FPdVEzd0F6X3BQWXBhem1xdmxjTwZDZD", // learn how to obtain it below
-        //userid = 3687936629, // User ID - get it in source HTML of your Instagram profile or look at the next example :)
-        num_photos = 8; // how much photos do you want to get
-    $.ajax({
-        url:
-            "https://graph.instagram.com/me/media?fields=id,username,media_url,thumbnail_url,permalink",
-        dataType: "jsonp",
-        data: { access_token: token, limit: num_photos },
-        type: "GET",
-        success: function (data) {
-            console.log(data);
-            for (x in data.data) {
-                $("#instagram-feed ul.instagram-photos").append(
-                    '<li class="instagram-item"><a class="instagram-link" href="' +
-                        data.data[x].permalink +
-                        '" target="_blank"><div class="instagram-photo-container"><div class="overlay"></div><img class="img-fluid instagram-photo px-0" src="' +
-                        data.data[x].media_url +
-                        '" alt="picture"></div><div class="instagram-view"><div class="fa fa-instagram"></div><p>View on Instagram</p></div></a></li>'
-                );
-            }
-            $(".instagram-photos").slick({
-                infinite: true,
-                speed: 1000,
-                arrows: false,
-                autoplay: true,
-                autoplaySpeed: 5000,
-                centerMode: true,
-                slidesToShow: 7,
-                slidesToScroll: 1,
-                responsive: [
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 5.5,
-                            slidesToScroll: 1,
-                        },
-                    },
-                    {
-                        breakpoint: 1024,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                        },
-                    },
-                    {
-                        breakpoint: 768,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1,
-                        },
-                    },
-                    {
-                        breakpoint: 480,
-                        settings: {
-                            slidesToShow: 1.3,
-                            slidesToScroll: 1,
-                        },
-                    },
-                    {
-                        breakpoint: 320,
-                        settings: {
-                            slidesToShow: 1.3,
-                            slidesToScroll: 1,
-                        },
-                    },
-                    {
-                        breakpoint: 1,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                        },
-                    },
-                ],
-            });
-        },
-        error: function (data) {
-            console.log(data); // send the error notifications to console
-        },
-    });
+  });
+  var token =
+      'IGQVJVS0J4Y3owZAG43OVFMQmkzZAi01ZAEpnYkp3d2wzTGFNTFl3ZA1pqZAVVLU2VaY0dHTmdjWnloZAW92Q3ItTlJTSllicEJsU1lCR0UtOVVUaVZALaE5vZAXNBUlY4dFd5QkRTdGI5dkM0SW5SaWI1QTZAFWAZDZD', // learn how to obtain it below
+    //userid = 3687936629, // User ID - get it in source HTML of your Instagram profile or look at the next example :)
+    num_photos = 8; // how much photos do you want to get
+  $.ajax({
+    url:
+      'https://graph.instagram.com/me/media?fields=id,username,media_url,thumbnail_url,permalink',
+    dataType: 'jsonp',
+    data: { access_token: token, limit: num_photos },
+    type: 'GET',
+    success: function (data) {
+      console.log(data);
+      for (x in data.data) {
+        $('#instagram-feed ul.instagram-photos').append(
+          '<li class="instagram-item"><a class="instagram-link" href="' +
+            data.data[x].permalink +
+            '" target="_blank"><div class="instagram-photo-container"><div class="overlay"></div><img class="img-fluid instagram-photo px-0" src="' +
+            data.data[x].media_url +
+            '" alt="picture"></div><div class="instagram-view"><div class="fa fa-instagram"></div><p>View on Instagram</p></div></a></li>'
+        );
+      }
+      $('.instagram-photos').slick({
+        infinite: true,
+        speed: 1000,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        centerMode: true,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 5.5,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1.3,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 320,
+            settings: {
+              slidesToShow: 1.3,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 1,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      });
+    },
+    error: function (data) {
+      console.log(data); // send the error notifications to console
+    },
+  });
 
-    equalheight = function (container) {
-        var currentTallest = 0,
-            currentRowStart = 0,
-            rowDivs = new Array(),
-            $el,
-            topPosition = 0;
-        $(container).each(function () {
-            $el = $(this);
-            $($el).height("auto");
-            topPosition = $el.position().top;
+  equalheight = function (container) {
+    var currentTallest = 0,
+      currentRowStart = 0,
+      rowDivs = new Array(),
+      $el,
+      topPosition = 0;
+    $(container).each(function () {
+      $el = $(this);
+      $($el).height('auto');
+      topPosition = $el.position().top;
 
-            if (currentRowStart != topPosition) {
-                for (
-                    currentDiv = 0;
-                    currentDiv < rowDivs.length;
-                    currentDiv++
-                ) {
-                    rowDivs[currentDiv].height(currentTallest);
-                }
-                rowDivs.length = 0;
-                currentRowStart = topPosition;
-                currentTallest = $el.height();
-                rowDivs.push($el);
-            } else {
-                rowDivs.push($el);
-                currentTallest =
-                    currentTallest < $el.height()
-                        ? $el.height()
-                        : currentTallest;
-            }
-            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-                rowDivs[currentDiv].height(currentTallest);
-            }
-        });
-        $(window).load(function () {
-            equalheight(".deck-item");
-        });
-        $(window).resize(function () {
-            equalheight(".deck-item");
-        });
-        $(window).width(function () {
-            equalheight(".deck-item");
-        });
-    };
+      if (currentRowStart != topPosition) {
+        for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+          rowDivs[currentDiv].height(currentTallest);
+        }
+        rowDivs.length = 0;
+        currentRowStart = topPosition;
+        currentTallest = $el.height();
+        rowDivs.push($el);
+      } else {
+        rowDivs.push($el);
+        currentTallest =
+          currentTallest < $el.height() ? $el.height() : currentTallest;
+      }
+      for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+        rowDivs[currentDiv].height(currentTallest);
+      }
+    });
+    $(window).load(function () {
+      equalheight('.deck-item');
+    });
+    $(window).resize(function () {
+      equalheight('.deck-item');
+    });
+    $(window).width(function () {
+      equalheight('.deck-item');
+    });
+  };
 });
